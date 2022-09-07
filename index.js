62846e92fd7e6320958ed9929a3363e5e3e70886
Thulane Mfeketho <mfekethothulane@gmail.comm>
2022-09-07 16:23:39 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 let button = document.getElementById("btn")
+let res = document.getElementById("res")
 
 button.addEventListener("click", function ageCal(){
     bornYear= document.getElementById("bornYear").value
     curYear = document.getElementById("currYear").value
-    document.getElementById("res").innerHTML = curYear-bornYear
+    sum = curYear-bornYear
+
+    if(curYear-bornYear){
+        alert(sum +" year old")
+    }
+   
 })
